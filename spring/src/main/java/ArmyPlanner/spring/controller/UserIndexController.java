d79c66e96ccf2296f47788cfe479c714741b4c6f
1233day <1233day@naver.com>
2023-06-03 13:07:37 +0900

    hotix: UserIndexController.java * 회원가입 시 userDto가 안넘어가는 문제 해결

--- spring/src/main/java/ArmyPlanner/spring/controller/UserIndexController.java
+++ spring/src/main/java/ArmyPlanner/spring/controller/UserIndexController.java
@@ -2,14 +2,16 @@ package ArmyPlanner.spring.controller;
 
 import ArmyPlanner.spring.domain.UserDto;
 import ArmyPlanner.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserIndexController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping("/auth/join")
     public String join() {
