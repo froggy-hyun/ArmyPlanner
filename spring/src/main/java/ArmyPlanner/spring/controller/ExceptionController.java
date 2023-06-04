8e3f017475a751614a2d72a0d2170a0fb0f41d64
1233day <1233day@naver.com>
2023-06-04 11:23:06 +0900

    feat: 접근 권한 없을 시 forbidden

---
+++ spring/src/main/java/ArmyPlanner/spring/controller/ExceptionController.java
@@ -0,0 +1,15 @@
+package ArmyPlanner.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ExceptionController {
+
+    @GetMapping("/forbidden")
+    public String exceptionHandling() {
+        return "forbidden";
+    }
+}
