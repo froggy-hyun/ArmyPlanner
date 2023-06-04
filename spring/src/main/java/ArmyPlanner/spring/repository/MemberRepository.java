2e4b39f04498482bad5e4109047bc089d2d4e76a
1233day <1233day@naver.com>
2023-06-04 14:35:24 +0900

    * fix: CrudRepository -> JpaRepository

--- spring/src/main/java/ArmyPlanner/spring/repository/MemberRepository.java
+++ spring/src/main/java/ArmyPlanner/spring/repository/MemberRepository.java
@@ -1,11 +1,12 @@
 package ArmyPlanner.spring.repository;
 
 import ArmyPlanner.spring.domain.Member;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUsername(String username);
 }
