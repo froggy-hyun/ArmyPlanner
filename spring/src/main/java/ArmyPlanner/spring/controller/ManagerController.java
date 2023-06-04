da973b34318bbca540d60381feb007d33502237d
1233day <1233day@naver.com>
2023-06-04 11:22:22 +0900

    fix: 어드민/매니저 페이지, 컨트롤러 구현

---
+++ spring/src/main/java/ArmyPlanner/spring/controller/ManagerController.java
@@ -0,0 +1,17 @@
+package ArmyPlanner.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/manager")
+public class ManagerController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "manager/managerPage";
+    }
+}
