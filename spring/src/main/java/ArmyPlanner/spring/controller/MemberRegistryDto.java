7ee75d02fdd431a45eb005402b48b39b24911abe
1233day <1233day@naver.com>
2023-06-06 16:37:16 +0900

    fix: 비밀번호 6자리 이상

--- spring/src/main/java/ArmyPlanner/spring/controller/MemberRegistryDto.java
+++ spring/src/main/java/ArmyPlanner/spring/controller/MemberRegistryDto.java
@@ -19,7 +19,7 @@ public class MemberRegistryDto {
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Length(min = 4, max = 16, message = "비밀번호는 4자 이상, 16자 이하로 입력해주세요.")
+    @Length(min = 6, max = 16, message = "비밀번호는 6자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
