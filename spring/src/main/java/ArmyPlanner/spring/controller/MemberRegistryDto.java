7d25ab8e889f3717eb3528e9b6f841ecce7d94e8
1233day <1233day@naver.com>
2023-06-06 18:07:56 +0900

    feat: 회원가입 정보 유효성 검사 강화

--- spring/src/main/java/ArmyPlanner/spring/controller/MemberRegistryDto.java
+++ spring/src/main/java/ArmyPlanner/spring/controller/MemberRegistryDto.java
@@ -8,6 +8,7 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @NoArgsConstructor
 @Data
@@ -24,8 +25,10 @@ public class MemberRegistryDto {
     private String password;
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{3,10}$", message = "이름은 특수문자 제외 3자이상 10자이하로 입력해주세요.")
     private String name;
 
+    @Pattern(regexp = "^^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$", message = "휴대폰 번호 양식이 아닙니다.(xxx-xxxx-xxxx)")
     private String phone;
 
     public Member toEntity() {
