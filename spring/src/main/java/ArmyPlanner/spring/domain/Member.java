d1c661b78fb0102e5ac05bdc963e81036024aacb
1233day <1233day@naver.com>
2023-06-06 17:55:46 +0900

    feat: 회원가입 정보 필수조건 강화

--- spring/src/main/java/ArmyPlanner/spring/domain/Member.java
+++ spring/src/main/java/ArmyPlanner/spring/domain/Member.java
@@ -20,19 +20,23 @@ public class Member{
 //    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
 //    @SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ")
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "member_id")
+    @Column(name = "member_id", nullable = false)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email; // 이메일
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String phone;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
     @CreationTimestamp
