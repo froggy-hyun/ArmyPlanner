f6b3878c6a21fc672406f8ebdb2df8a19c62d3d4
1233day <123day@naver.com>
2023-06-09 20:31:14 +0900

    feat: 플래너 event entity 생성 * Event.java

---
+++ spring/src/main/java/ArmyPlanner/spring/domain/Event.java
@@ -0,0 +1,28 @@
+package ArmyPlanner.spring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="event")
+@Getter @Setter
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private LocalDateTime start;
+    @Column(nullable = false)
+    private LocalDateTime finish;
+
+}
