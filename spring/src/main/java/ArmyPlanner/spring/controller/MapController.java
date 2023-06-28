346e335840be536deb1f7e6d8a5b98409311f49d
1233day <1233day@naver.com>
2023-06-28 19:23:31 +0900

    feat: 군 복지 휴양 시설 지도 표시 기초 구현

--- spring/src/main/java/ArmyPlanner/spring/controller/MapController.java
+++ spring/src/main/java/ArmyPlanner/spring/controller/MapController.java
@@ -3,7 +3,15 @@ package ArmyPlanner.spring.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +26,36 @@ public class MapController {
     @GetMapping("armyFacilityView")
     public String armyFacilityView(){
         return "map/map_armyFacilityView";
+    }
+
+    @GetMapping("armyFacilityView/getArmyFacilityApi/{start}/{end}")
+    @ResponseBody
+    public String getPxApi(@PathVariable Long start, @PathVariable Long end) throws IOException {
+        String serviceKey = "3733313631313630353532323832313332";
+        String resultType = "json";
+        String service = "DS_WHLAM_WLFR_VCTNINSTLT";
+        String string_url = "https://openapi.mnd.go.kr/" + serviceKey + "/" + resultType + "/" + service + "/" + start + "/" + end;
+        URL url = new URL(string_url);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        BufferedReader rd;
+
+        // 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+        return sb.toString();
+
     }
 
     @GetMapping("armyHospitalView")
