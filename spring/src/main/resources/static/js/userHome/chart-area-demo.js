0f169ee3ff9e535eb7cfe0c5108bcedf115d7698
1233day <1233day@naver.com>
2023-07-03 20:54:48 +0900

    feat: 소비 차트 틀 구현

--- spring/src/main/resources/static/js/userHome/chart-area-demo.js
+++ spring/src/main/resources/static/js/userHome/chart-area-demo.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["6일전", "5일전", "4일전", "3일전", "2일전", "어제", "오늘"],
     datasets: [{
       label: "Sessions",
       lineTension: 0.3,
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [31274, 33259, 25849, 24159, 32651, 31984, 38451],
     }],
   },
   options: {
--- spring/src/main/resources/static/js/userHome/chart-bar-demo.js
+++ spring/src/main/resources/static/js/userHome/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["2달전", "저번달", "이번달"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [7841, 9821, 14984],
     }],
   },
   options: {
